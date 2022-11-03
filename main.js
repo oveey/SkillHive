2198d699c658e73efa9adcb50774ae8ca56b2b9c
oveey <ovieigere@gmail.com>
2022-11-03 22:17:32 +0100

    fixed nav bar

--- main.js
+++ main.js
@@ -25,6 +25,10 @@ const menu = document.querySelector(".nav_menu");
 const menuBtn = document.querySelector("#open-menu-btn");
 const closeBtn = document.querySelector("#close-menu-btn");
 
+// menu.style.display = "none";
+// closeBtn.style.display = "none";
+// menuBtn.style.display = "inline-block";
+
 menuBtn.addEventListener('click', () => {
   menu.style.display = "flex";
   closeBtn.style.display = "inline-block";
